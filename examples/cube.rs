79092b8bcb45565c560238f2b58773c646969958
Jack Bass <jeb4571@rit.edu>
2020-08-01 20:42:04 -0400

    Fixed triangle example, committed cube example to examples folder

--- examples/cube.rs
+++ examples/cube.rs
@@ -9,12 +9,44 @@ impl Triangle {
         use rise::point;
 
         let vertices = vec!(
-            point!(0.0, 0.5, -1.0),
-            point!(-0.5, -0.5, -1.0),
-            point!(0.5, -0.5, -1.0)
+            point!(-1., -1., 1.),
+            point!(1., -1., 1.),
+            point!(1., 1., 1.),
+            point!(-1., 1., 1.),
+
+            point!(-1., 1., -1.),
+            point!(1., 1., -1.),
+            point!(1., -1., -1.),
+            point!(-1., -1., -1.),
+
+            point!(1., -1., -1.),
+            point!(1., 1., -1.),
+            point!(1., 1., 1.),
+            point!(1., -1., 1.),
+
+            point!(-1., -1., 1.),
+            point!(-1., 1., 1.),
+            point!(-1., 1., -1.),
+            point!(-1., -1., -1.),
+
+            point!(1., 1., -1.),
+            point!(-1., 1., -1.),
+            point!(-1., 1., 1.),
+            point!(1., 1., 1.),
+
+            point!(1., -1., 1.),
+            point!(-1., -1., 1.),
+            point!(-1., -1., -1.),
+            point!(1., -1., -1.),
         );
 
-        let indices : Vec<u16> = vec!(0, 1, 2);
+        let indices : Vec<u16> = vec!(0, 1, 2, 2, 3, 0, // top
+            4, 5, 6, 6, 7, 4, // bottom
+            8, 9, 10, 10, 11, 8, // right
+            12, 13, 14, 14, 15, 12, // left
+            16, 17, 18, 18, 19, 16, // front
+            20, 21, 22, 22, 23, 20, // back
+        );
         
         let mut mesh = rise::graphics::Mesh::new();
 
